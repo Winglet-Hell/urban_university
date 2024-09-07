cb6dd7a05b7536879c8a56919a58c59afb98942f
Winglet-Hell <i@stpra.ru>
2024-09-07 15:29:23 +0300

    Add Multiply3 class with multiplication and string representation methods

--- drafts/new.py
+++ drafts/new.py
@@ -1,13 +1,23 @@
-# класс который будет принимать имя и возраст и здороваться с этим человеком
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.greet()
+class Multiply3:
+    def __init__(self, x):
+        self.x = x
 
-    def greet(self):
-        print(f"Hello, {self.name}! You are {self.age} years old.")
+    def __mul__(self, other):
+        if isinstance(other, Multiply3):
+            return self.x * other.x * 3
+
+    def __str__(self):
+        return str(self.x)
+
+    def __repr__(self):
+        return str(self.x)
 
 
-# создаем объект класса Person
-person = Person("Alice", 25)
+m1 = Multiply3(10)
+m2 = Multiply3(3)
+m3 = Multiply3(4)
+
+print(m1 * m3)  # 18
+print(m1)  # 2
+print(m2)  # 3
+print(2 * 3)
